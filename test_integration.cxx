2bc6d975d049efc3f7b7ad71da0db7a2af5cb8ac
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-04-17 21:27:12 -0400

    class21s: start using MPI boilerplate for test_integration_mpi
    * init/finalize MPI
    * use MPI_Wtime()

--- test_integration.cxx
+++ test_integration.cxx
@@ -2,7 +2,7 @@
 #include <cmath>
 #include <iostream>
 
-#include "wtime.h"
+#include <mpi.h>
 
 double f(double x) { return std::sqrt(1. - x * x); }
 
@@ -10,16 +10,18 @@ int main(int argc, char** argv)
 {
   const int N = 100000000;
 
-  double t1 = Wtime();
+  MPI_Init(&argc, &argv);
+  double t1 = MPI_Wtime();
   double sum = 0.;
   double dx = 1. / N;
   for (int i = 0; i < N; i++) {
     sum += .5 * dx * (f(i * dx) + f((i + 1) * dx));
   }
-  double t2 = Wtime();
+  double t2 = MPI_Wtime();
   std::cout << "time " << t2 - t1 << " secs\n";
 
   std::cout << "integral is approximately " << sum << "\n";
 
+  MPI_Finalize();
   return 0;
 }
