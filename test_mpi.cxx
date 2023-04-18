4bcc2c7457c69af30dc6ac9b1d3841bc8d422dde
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-04-17 21:17:35 -0400

    class20s: print rank, size, etc.

--- test_mpi.cxx
+++ test_mpi.cxx
@@ -1,8 +1,18 @@
 
 #include <iostream>
+#include <mpi.h>
 
 int main(int argc, char** argv)
 {
-  std::cout << "Hello, non-MPI!\n";
+  std::cout << "starting!\n";
+  MPI_Init(&argc, &argv);
+
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  std::cout << "rank " << rank << " / " << size << "\n";
+
+  MPI_Finalize();
+  std::cout << "done!\n";
   return 0;
 }
