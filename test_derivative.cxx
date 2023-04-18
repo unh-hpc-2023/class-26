1cc6df17c5bf6bf8d4e79fe77e9657905a0b7377
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-04-17 21:41:58 -0400

    Revert "class22: use xtensor's periodic()"
    This reverts commit 66e166342ab65d8769fb627128368a74df42abab.

--- test_derivative.cxx
+++ test_derivative.cxx
@@ -11,8 +11,8 @@ xt::xtensor<double, 1> derivative(const xt::xtensor<double, 1>& f, double dx)
   auto fprime = xt::zeros_like(f);
 
   int N = f.shape(0);
-  for (int i = 0; i < N; i++) {
-    fprime(i) = (f.periodic(i + 1) - f.periodic(i - 1)) / (2. * dx);
+  for (int i = 1; i < N - 1; i++) {
+    fprime(i) = (f(i + 1) - f(i - 1)) / (2. * dx);
   }
 
   return fprime;
