d1552ac34117afd351e1ca7e8e2454613f6cc056
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-04-17 21:30:22 -0400

    class21s: distribute the work (trapezoids) amongst available MPI processes
    Each process will print their partial result now, what's missing is
    collecting them all and calculating the final result.

--- test_integration.cxx
+++ test_integration.cxx
@@ -1,4 +1,5 @@
 
+#include <cassert>
 #include <cmath>
 #include <iostream>
 
@@ -11,10 +12,17 @@ int main(int argc, char** argv)
   const int N = 100000000;
 
   MPI_Init(&argc, &argv);
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  assert(N % size == 0); // only support evenly divisible number of trapezoids
+  int ib = rank * (N / size);
+  int ie = (rank + 1) * (N / size);
+
   double t1 = MPI_Wtime();
   double sum = 0.;
   double dx = 1. / N;
-  for (int i = 0; i < N; i++) {
+  for (int i = ib; i < ie; i++) {
     sum += .5 * dx * (f(i * dx) + f((i + 1) * dx));
   }
   double t2 = MPI_Wtime();
