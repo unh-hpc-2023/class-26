ca99a57de8d10f8591760072122ee226ff303df1
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-04-17 21:47:50 -0400

    class22: show a fancier xtensor way of calculating the derivative

--- test_derivative.cxx
+++ test_derivative.cxx
@@ -16,12 +16,9 @@ xt::xtensor<double, 1> derivative(const xt::xtensor<double, 1>& f, double dx)
   f_g(G + -1) = f_g(G + N - 1);
   f_g(G + N) = f_g(G + 0);
 
-  auto fprime = xt::zeros_like(f);
-  for (int i = 0; i < N; i++) {
-    fprime(i) = (f_g(i + G + 1) - f_g(i + G - 1)) / (2. * dx);
-  }
-
-  return fprime;
+  // Python equivalent: return (f_g[2:] - f_g[:-2]) / (2. * dx);
+  return (xt::view(f_g, xt::range(2, _)) - xt::view(f_g, xt::range(_, -2))) /
+         (2. * dx);
 }
 
 int main(int argc, char** argv)
