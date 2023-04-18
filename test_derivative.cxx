b6c2d6d0edb90bcbdee079a584ee14b63503ec96
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-04-17 21:40:38 -0400

    class22: calculate the derivative, inner points only
    because if I were to do this on the boundary points, I'd access f
    out of bounds.

--- test_derivative.cxx
+++ test_derivative.cxx
@@ -6,15 +6,31 @@
 #include <fstream>
 #include <iostream>
 
+xt::xtensor<double, 1> derivative(const xt::xtensor<double, 1>& f, double dx)
+{
+  auto fprime = xt::zeros_like(f);
+
+  int N = f.shape(0);
+  for (int i = 1; i < N - 1; i++) {
+    fprime(i) = (f(i + 1) - f(i - 1)) / (2. * dx);
+  }
+
+  return fprime;
+}
+
 int main(int argc, char** argv)
 {
   // create coordinates [0, 2pi)
   auto x = xt::linspace<double>(0., 2. * M_PI, 16, false);
+  double dx = x(1) - x(0);
   // our original function f
   auto f = sin(x);
 
+  // calculate derivative
+  auto fprime = derivative(f, dx);
+
   std::ofstream out("f.csv");
-  xt::dump_csv(out, xt::stack(xt::xtuple(x, f), 1));
+  xt::dump_csv(out, xt::stack(xt::xtuple(x, f, fprime), 1));
 
   return 0;
 }
