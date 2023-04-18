1101b54a6c72b8a87657f4012279ca48ccca7036
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-04-17 21:43:59 -0400

    class22: special case derivative near boundary
    That's one typical approach, it's just a bit of pain because one has to
    repeat the calculation in special variations at both boundaries, which
    is not really an issue for a simple central difference, but for more
    complicated calculations, it's not so much fun...

--- test_derivative.cxx
+++ test_derivative.cxx
@@ -8,12 +8,14 @@
 
 xt::xtensor<double, 1> derivative(const xt::xtensor<double, 1>& f, double dx)
 {
-  auto fprime = xt::zeros_like(f);
-
   int N = f.shape(0);
+
+  auto fprime = xt::zeros_like(f);
+  fprime(0) = (f(1) - f(N - 1)) / (2. * dx);
   for (int i = 1; i < N - 1; i++) {
     fprime(i) = (f(i + 1) - f(i - 1)) / (2. * dx);
   }
+  fprime(N - 1) = (f(0) - f(N - 2)) / (2. * dx);
 
   return fprime;
 }
