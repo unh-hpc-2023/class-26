40cb8eacedeae12f80a23e8aa87e4fe5668c0d65
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-04-24 22:26:58 -0400

    class22s: use `n` for local domain size
    rather than `N`, which was used for the global domain size. In some
    sense, this just makes it more obvious that the boundary condition we're
    setting is wrong :(

--- test_derivative.cxx
+++ test_derivative.cxx
@@ -13,15 +13,15 @@
 xt::xtensor<double, 1> derivative(const xt::xtensor<double, 1>& f, double dx)
 {
   const int G = 1;
-  int N = f.shape(0);
+  int n = f.shape(0);
 
   auto f_g = xt::pad(f, G);
-  f_g(G + -1) = f_g(G + N - 1);
-  f_g(G + N) = f_g(G + 0);
+  f_g(G + -1) = f_g(G + n - 1);
+  f_g(G + n) = f_g(G + 0);
 
   auto fprime = xt::zeros_like(f);
-  for (int i = 0; i < N; i++) {
-    fprime(i) = (f_g(i + G + 1) - f_g(i + G - 1)) / (2. * dx);
+  for (int i = 0; i < n; i++) {
+    fprime(i) = (f_g(G + i + 1) - f_g(G + i - 1)) / (2. * dx);
   }
 
   return fprime;
