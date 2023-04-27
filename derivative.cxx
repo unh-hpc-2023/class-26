85cb05239e67b4e903fc2fd65fe3bafed170d428
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-04-27 09:21:25 -0400

    class23s: introduce a separate fill_ghosts() function
    still just does periodic b.c., ie., doesn't work right in parallel

--- derivative.cxx
+++ derivative.cxx
@@ -3,14 +3,21 @@
 
 #include <xtensor/xpad.hpp>
 
+void fill_ghosts(xt::xtensor<double, 1>& f_g)
+{
+  const int G = 1;
+  int n = f_g.shape(0) - 2 * G;
+  f_g(G + -1) = f_g(G + n - 1);
+  f_g(G + n) = f_g(G + 0);
+}
+
 xt::xtensor<double, 1> derivative(const xt::xtensor<double, 1>& f, double dx)
 {
   const int G = 1;
   int n = f.shape(0);
 
   auto f_g = xt::pad(f, G);
-  f_g(G + -1) = f_g(G + n - 1);
-  f_g(G + n) = f_g(G + 0);
+  fill_ghosts(f_g);
 
   auto fprime = xt::zeros_like(f);
   for (int i = 0; i < n; i++) {
