0d24869359014c3df333d26c57c435053c655936
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-05-04 09:55:41 -0400

    kdv: implement actual kdv equation

--- kdv.cxx
+++ kdv.cxx
@@ -9,30 +9,35 @@
 #include <iostream>
 
 xt::xtensor<double, 1> rhs(const mpi_domain& domain,
-                           const xt::xtensor<double, 1>& f)
+                           const xt::xtensor<double, 1>& u)
 {
-  const int G = 1;
+  const int G = 2;
   int n = domain.n();
-  assert(f.shape(0) == n);
+  assert(u.shape(0) == n);
 
-  auto f_g = xt::pad(f, G);
-  domain.fill_ghosts(f_g);
+  auto u_g = xt::pad(u, G);
+  domain.fill_ghosts(u_g);
 
-  auto fprime = xt::zeros_like(f);
+  double dx3i = 1. / (2 * std::pow(domain.dx(), 3));
+  double dxi = 1. / (2. * domain.dx());
+
+  auto rhs = xt::zeros_like(u);
   for (int i = 0; i < n; i++) {
-    fprime(i) = (f_g(G + i + 1) - f_g(G + i + -1)) / (2. * domain.dx());
+    rhs(i) = (u_g(G + i + 2) - 2 * u_g(G + i + 1) + 2 * u_g(G + i - 1) -
+              u_g(G + i - 2)) *
+               dx3i -
+             6 * u_g(G + i) * (u_g(G + i + 1) - u_g(G + i - 1)) * dxi;
   }
 
-  return fprime;
+  return rhs;
 }
 
 int main(int argc, char** argv)
 {
-  const int N = 16;            // number of grid points
-  const double L = 2. * M_PI;  // total size of domain
-  const int n_timesteps = 100; // total number of steps to run
-  const int output_every = 1;  // how often to write output
-  const double c = 1;          // wave speed
+  const int N = 128;              // number of grid points
+  const double L = 2. * M_PI;     // total size of domain
+  const int n_timesteps = 100000; // total number of steps to run
+  const int output_every = 1000;  // how often to write output
 
   MPI_Init(&argc, &argv);
   mpi_domain domain(MPI_COMM_WORLD, N, L);
@@ -41,19 +46,27 @@ int main(int argc, char** argv)
   auto x = domain.coords();
 
   // our initial condition
+#if 0
   xt::xtensor<double, 1> u = sin(x);
+#else
+  double c = 3.;
+  xt::xtensor<double, 1> u =
+    -.5 * c / xt::pow(xt::cosh(.5 * std::sqrt(c) * (x - M_PI)), 2);
+#endif
 
-  double dt = domain.dx();
+  // a very stringent CLF condition
+  double dt = .1 * std::pow(domain.dx(), 3);
   for (int n = 0; n < n_timesteps; n++) {
     if (n % output_every == 0) {
+      std::cout << "writing output " << n << "\n";
       std::ofstream out("u-" + std::to_string(n) + "-" +
                         std::to_string(domain.rank()) + ".csv");
       xt::dump_csv(out, xt::stack(xt::xtuple(x, u), 1));
     }
 
     // advance one timestep
-    auto k1 = -c * rhs(domain, u);
-    auto k2 = -c * rhs(domain, u + .5 * dt * k1);
+    auto k1 = rhs(domain, u);
+    auto k2 = rhs(domain, u + .5 * dt * k1);
     u += dt * k2;
   }
 
